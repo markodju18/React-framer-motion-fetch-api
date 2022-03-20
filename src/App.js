705c03076dc31d617e99b464c8927d9549db68fc
Marko Djuric <markodjuric037@gmail.com>
2022-03-20 18:52:02 +0100

    Movies filtering app

---
+++ src/App.js
@@ -0,0 +1,37 @@
+
+import './App.css';
+import {useEffect, useState} from 'react';
+import Movie from './Movie';
+import Filter from './Filter';
+import {motion} from 'framer-motion';
+
+function App() {
+
+  const [popular, setPopular] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [activeGenre, setActiveGenre] = useState(0);
+
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+
+  const fetchPopular = async () => {
+    const data = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=7e1e23477a1818719e2512cf4c4a17e4&language=en-US&page=1");
+    const movies = await data.json();
+    setPopular(movies.results);
+    setFiltered(movies.results);
+  }
+
+  return (
+    <div className="App">
+      <Filter popular={popular} setFiltered={setFiltered} activeGenre={activeGenre} setActiveGenre={setActiveGenre} />
+      <motion.div layout className='popular-movies'>
+        {filtered.map((movie) => {
+          return <Movie key={movie.id} movie={movie} /> 
+        })}
+      </motion.div>
+    </div>
+  );
+}
+
+export default App;
